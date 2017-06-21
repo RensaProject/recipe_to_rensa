0eb378aa1c62dda330492eedcc75fffd6bb8e50f
Sarah H <smharmon@ucsc.edu>
2017-06-21 00:39:49 -0400

    init

---
+++ README.md
@@ -0,0 +1,24 @@
+# Recipe to Rensa
+
+## Overview
+Extract ingredients from a recipe written in English. Store as Rensa encodings (Simprel format).  
+
+(Recipe instructions can also be parsed and the Simprel JSON structures can be realized - just ask if you're interested!)
+
+## Requirements
+This code requires [rensabrain](https://github.com/RensaProject/rensabrain) and ingredient parser.  The latter can be installed with pip:
+```
+pip install ingredient_parser
+```
+
+## License
+If you use this repository, please cite:
+
+```
+@phdthesis{harmon2017narrative,
+  title={Narrative Encoding for Computational Reasoning and Adaptation},
+  author={Harmon, Sarah M},
+  year={2017},
+  school={University of California, Santa Cruz}
+}
+```
---
+++ main.py
@@ -0,0 +1,58 @@
+'''
+Simple ingredient extraction into a Rensa brain (Simprel format).
+'''
+
+from ingredient_parser.en import parse
+from rensabrain.Brain import *
+
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+
+def write_recipe_assertion(line, parsed):
+    if line != "":
+        try:
+            if "," in parsed["name"]:
+                left = parsed["name"].split(", ")[0].lower()
+            else:
+                left = parsed["name"].lower()
+            meas = parsed["measure"]
+            num_idx = len(meas)-1
+            for c in reversed(meas):
+                if is_number(c):
+                    break;
+                num_idx-=1;
+
+            right_unit = parsed["measure"][num_idx+2:]
+            right_num = parsed["measure"][:num_idx+1]
+            comment = parsed["name"].split(", ")[1:]
+
+            return {
+                "l":[left],
+                "relation":"instance_of",
+                "r":["ingredient"], "r_unit":[right_unit],
+                "r_num":[right_num],
+                "comment": comment,
+                "text": line
+            }
+        except:
+            return None
+    return None
+
+def main():
+    assertions = []
+    with open('input/recipe.txt', 'r') as myfile:
+        s=myfile.read()
+    for line in s.split("\n"):
+        assertion = write_recipe_assertion(line, parse(line))
+        if assertion is not None:
+            assertions.append(assertion)
+    recipe = make_brain(assertions)
+    recipe.save_brain("assertions/")
+    delete_old_assertions("assertions/")
+
+if __name__ == '__main__':
+    main()
